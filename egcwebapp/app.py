ce34b015c7d68b204a1c4f344dd6c87b84d8982e
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2023-04-11 16:31:41 +0200

    🔧 chore(app.py): refactor API routes to separate HTML and JSON routes 🔧 chore(app.py): add debug mode for development environment 🔧 chore(app.py): refactor ExtractForm to use TextAreaField for contents input

--- egcwebapp/app.py
+++ egcwebapp/app.py
@@ -6,9 +6,13 @@ from pathlib import Path
 import os
 
 app = Flask(__name__)
+egc_data = None
 app.secret_key = 'secret_key'
 app.config['UPLOAD_FOLDER'] = str(Path(app.instance_path) / 'uploads')
-egc_data = None
+if os.environ.get('EGCWEBAPP') == 'development':
+  app.debug = True
+  this_dir = Path(__file__).parent
+  egc_data = EGCData.from_file(str(this_dir.parent / "archaea.egc"))
 
 # The following web routes are defined for each type of record:
 #
@@ -18,12 +22,14 @@ egc_data = None
 # U /<record_type>s/<record_id>/edit    page for editing a single record
 # D /<record_type>s/<record_id>/delete  deletes a single record
 #
-# Furthermore, the following API routes are defined. These are used by the
-# JavaScript code in the application. They return JSON if the request header
-# 'Accept' is set to 'application/json', otherwise they return tabular HTML.
+# Furthermore, the following API routes are defined. The /api/json/<> routes
+# return JSON data, while the /api/<> routes return HTML tables.
 #
 # /api/<record_type>s/<record_id>                        single record
 # /api/<record_type>s/<record_id>/<ref_by_record_type>
+#                     record from which the given record is referenced
+# /api/json/<record_type>s/<record_id>                   single record
+# /api/json/<record_type>s/<record_id>/<ref_by_record_type>
 #                     record from which the given record is referenced
 
 @app.route('/')
@@ -118,17 +124,31 @@ def show_document(record_id):
         abort(404)
     return render_template('show_document.html', document=document)
 
+@app.route('/api/json/documents/<record_id>', methods=['GET'])
+def get_document_json(record_id):
+  if egc_data is None:
+    return redirect(url_for('load_egc_file'))
+  else:
+    document = egc_data.get_record_by_id(record_id)
+    return jsonify(document)
+
 @app.route('/api/documents/<record_id>', methods=['GET'])
 def get_document(record_id):
   if egc_data is None:
     return redirect(url_for('load_egc_file'))
   else:
     document = egc_data.get_record_by_id(record_id)
-    if request.accept_mimetypes.accept_json:
-      return jsonify(document)
-    else:
-      return render_template('table_show_document.html', document=document,
-              egc_data=egc_data)
+    return render_template('table_show_document.html', document=document,
+            egc_data=egc_data)
+
+@app.route('/api/json/documents/<record_id>/extracts', methods=['GET'])
+def get_document_extracts_json(record_id):
+  if egc_data is None:
+    return redirect(url_for('load_egc_file'))
+  else:
+    extracts = egc_data.ref_by('D', record_id, 'S') + \
+               egc_data.ref_by('D', record_id, 'T')
+    return jsonify(extracts)
 
 @app.route('/api/documents/<record_id>/extracts', methods=['GET'])
 def get_document_extracts(record_id):
@@ -137,11 +157,8 @@ def get_document_extracts(record_id):
   else:
     extracts = egc_data.ref_by('D', record_id, 'S') + \
                egc_data.ref_by('D', record_id, 'T')
-    if request.accept_mimetypes.accept_json:
-      return jsonify(extracts)
-    else:
-      return render_template('extract_sub_list.html', extracts=extracts,
-          egc_data=egc_data, parent_id=record_id)
+    return render_template('extract_sub_list.html', extracts=extracts,
+        egc_data=egc_data, parent_id=record_id)
 
 @app.route('/extracts')
 def extract_list():
@@ -157,7 +174,7 @@ def create_extract():
   if egc_data is None:
     return redirect(url_for('load_egc_file'))
   else:
-    form = ExtractForm(request.form)
+    form = ExtractForm(request.form, egc_data=egc_data)
     if request.method == 'POST' and form.validate():
         new_record = {
             "record_type": form.record_type.data,
@@ -182,35 +199,39 @@ def edit_extract(record_id):
   if egc_data is None:
     return redirect(url_for('load_egc_file'))
   else:
-    extract = egc_data.get_extract(record_id)
+    extract = egc_data.get_record_by_id(record_id)
     if extract is None:
         abort(404)
 
-    form = ExtractForm(request.form,
+  if extract["record_type"] == "T":
+      contents = extract["table_ref"]
+  elif extract["record_type"] == "S":
+      contents = extract["text"]
+  form = ExtractForm(request.form,
         egc_data=egc_data, old_id = record_id,
         data={
+          "id": extract["id"],
           "record_type": extract["record_type"],
           "document_id": extract["document_id"]["item"],
-          "id": extract["id"],
-          "contents": extract.get("contents", ""),
+          "contents": contents,
         })
-    if request.method == 'POST' and form.validate():
-        updated_data = {
-            "record_type": form.record_type.data,
-            "id": form.id.data,
-            "document_id": {
-                "resource_prefix": "PMID",
-                "item": form.document_id.data,
-                "location": None,
-                "term": None
-            }}
-        if form.record_type.data == "T":
-            updated_data["table_ref"] = form.contents.data
-        elif form.record_type.data == "S":
-            updated_data["text"] = form.contents.data
-        egc_data.update_record_by_id(record_id, updated_data)
-        return redirect(url_for('extract_list'))
-    return render_template('edit_extract.html', form=form)
+  if request.method == 'POST' and form.validate():
+    updated_data = {
+        "record_type": form.record_type.data,
+        "id": form.id.data,
+        "document_id": {
+            "resource_prefix": "PMID",
+            "item": form.document_id.data,
+            "location": None,
+            "term": None
+        }}
+    if form.record_type.data == "T":
+        updated_data["table_ref"] = form.contents.data
+    elif form.record_type.data == "S":
+        updated_data["text"] = form.contents.data
+    egc_data.update_record_by_id(record_id, updated_data)
+    return redirect(url_for('extract_list'))
+  return render_template('edit_extract.html', form=form)
 
 @app.route('/extracts/<record_id>/delete', methods=['POST'])
 def delete_extract(record_id):
--- egcwebapp/forms.py
+++ egcwebapp/forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, StringField, SelectField, validators
+from wtforms import Form, StringField, SelectField, TextAreaField, validators
 
 class DocumentForm(Form):
 
@@ -19,26 +19,26 @@ class DocumentForm(Form):
         self.old_id = kwargs.pop('old_id', None)
 
 class ExtractForm(Form):
-    record_id = StringField('Record ID',
+    id = StringField('Record ID',
         [validators.Length(min=1, max=50), validators.DataRequired()])
     record_type = SelectField('Record Type',
         choices=[('S', 'Snippet'), ('T', 'Table')],
         validators=[validators.DataRequired()])
     document_id = StringField('Document ID',
         [validators.Length(min=1, max=50), validators.DataRequired()])
-    content = StringField('Text / Table Ref')
+    contents = TextAreaField('Text / Table Ref', render_kw={"rows": 10})
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.egc_data = kwargs.pop('egc_data')
         self.old_id = kwargs.pop('old_id', None)
 
-    def validate_record_id(self, field):
+    def validate_id(self, field):
         if not self.egc_data.has_unique_id(field.data):
             raise validators.ValidationError('Record ID already exists')
 
     def validate_document_id(self, field):
-        d_record_id = self.egc_data.compute_docid(field.data)
-        if not self.egc_data.id_exists(d_record_id):
+        d_id = self.egc_data.compute_docid(field.data)
+        if not self.egc_data.id_exists(d_id):
             raise validators.ValidationError('Document ID does not exist')
 
