af6d9491dc0f66b49001dce47542d947b69fa923
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2023-04-20 01:22:07 +0200

    normalize extract -> document link

--- egcwebapp/static/js/list_extract.mjs
+++ egcwebapp/static/js/list_extract.mjs
@@ -1,22 +1,8 @@
 import { initTooltip } from "./tooltips.js";
+import { initRelatedTooltips } from "./tooltips.js";
 
 export async function initTooltips() {
-  $(".document-info").each(async function() {
-    const documentId = $(this).prev().data('document-id');
-    try {
-      const response = await fetch(`/api/documents/${documentId}`);
-      if (!response.ok) {
-        console.error(`Failed to fetch D record information for document ${documentId}`);
-        return "<p>Error: Failed to fetch D record information</p>";
-      }
-      const table = await response.text();
-      const html = `<div class="tooltip-table">${table}</div>`
-      initTooltip($(this), "D record", html);
-    } catch (error) {
-      console.error(`Failed to fetch D record information for document ${documentId}:`, error);
-      return "<p>Error: Failed to fetch D record information</p>";
-    }
-  });
+  initRelatedTooltips(".document-info", "documents");
 }
 
 $(document).ready(function() {
