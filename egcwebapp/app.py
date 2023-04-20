669bb83ff84ccceb1d685351ce93bdad05d18183
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2023-04-20 15:38:54 +0200

    move record_kinds_info to own module

--- egcwebapp/app.py
+++ egcwebapp/app.py
@@ -62,33 +62,7 @@ def create_app():
           os.path.basename(egc_data.file_path),
           mimetype='application/egc')
 
-  record_kind_info = {
-      'document': {"title": 'Document',
-                   "record_types": ['D'],
-                   "ref_by_kinds": ['extract']},
-      'extract': {"title": 'Document Extract',
-                  "record_types": ['S', 'T'],
-                  "ref_by_kinds": ['crule', 'vrule']},
-      'unit': {"title": "Unit",
-               "record_types": ['U'],
-               "ref_by_kinds": ['unit', 'attribute', 'model']},
-      'attribute': {"title": "Attribute",
-                    "record_types": ['A'],
-                    "ref_by_kinds": ['vrule', 'crule']},
-      'group': {"title": "Group",
-                "record_types": ['G'],
-                "ref_by_kinds": ['group', 'vrule', 'crule']},
-      'model': {"title": "Model",
-                "record_types": ['M'],
-                "ref_by_kinds": []},
-      'vrule': {"title": "Value Expectation Rule",
-                "record_types": ['V'],
-                "ref_by_kinds": []},
-      'crule': {"title": "Comparative Expectation Rule",
-                "record_types": ['C'],
-                "ref_by_kinds": []},
-  }
-
+  from egcwebapp.record_kinds import record_kind_info
   record_kinds = list(record_kind_info.keys())
 
   def list_route(record_kind):
